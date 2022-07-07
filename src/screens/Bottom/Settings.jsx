a21cf391412cece4336705b82d2c26615ae6b389
dianKoliva <dianpretty01@gmail.com>
2022-07-07 09:28:21 +0200

    new changes

--- src/screens/Bottom/Settings.jsx
+++ src/screens/Bottom/Settings.jsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Settings = () => {
   return (
     <View>
-      <Text>This is the settings screen</Text>
+
+      <View style={styles.logcontainer}>
+      <TouchableOpacity style={styles.logout}>
+      <Text style={{fontWeight:"bold",color:"white"}}>Click here to logout</Text>
+      </TouchableOpacity>
+      </View>
+     
+
     </View>
   )
 }
 
 export default Settings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  logout:{
+  backgroundColor:"tomato",
+  padding:25,
+  
+  },
+  logcontainer:{
+    height:"100%",
+    justifyContent:"center",
+    alignItems:"center",
+  }
+})
